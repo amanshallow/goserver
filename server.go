f60db3f0614163b6a6ac4195d4176ebaf6a028be
Aman <63730225+asingh2Oswego@users.noreply.github.com>
2020-10-02 21:58:31 -0400

    HTTP Status Error Improvements
    Improved error handling and how HTTP status codes were returned to user for unauthorized paths and HTTP methods.

--- server.go
+++ server.go
@@ -15,10 +15,6 @@ type Message struct {
 	Time string
 }
 
-type Error struct {
-	Status int
-}
-
 // Returns HTTP status codes, pushes notifications to Loggly
 func status(w http.ResponseWriter, r *http.Request) {
     var tag string
@@ -33,15 +29,17 @@ func status(w http.ResponseWriter, r *http.Request) {
         	client.Send("error", "Could not retrieve client's IP address.")
 	}
     	
-    if r.Method != "GET" {
+    if r.Method != http.MethodGet {
     	client.EchoSend("warn", "Method: " + r.Method + ", Host: " + ip + ":" + port +  ", Requested Path: " + r.URL.Path + ", HTTP Status Code: " 	+ strconv.Itoa(http.StatusMethodNotAllowed) + ".")
-    	data := Error{http.StatusMethodNotAllowed}
-    	json.NewEncoder(w).Encode(data)
+	w.WriteHeader(http.StatusMethodNotAllowed)
+	data := "405 - Method Not Allowed."
+	json.NewEncoder(w).Encode(data)
     } else {
     	if r.URL.Path != "/status" {
     		client.EchoSend("info", "Method: " + r.Method + ", Host: " + ip + ":" + port +  ", Requested Path: " + r.URL.Path + ", HTTP Status Code: " + strconv.Itoa(http.StatusNotFound) + ".")
-    		data := Error{http.StatusNotFound}
-    		json.NewEncoder(w).Encode(data)
+		w.WriteHeader(http.StatusNotFound)
+		data := "404 - Page Not Found."
+		json.NewEncoder(w).Encode(data)
     	} else {
     	    	client.EchoSend("info", "Method: " + r.Method + ", Host: " + ip + ":" + port +  ", Requested Path: " + r.URL.Path + ", HTTP Status Code: " + strconv.Itoa(http.StatusOK) + ".")
 	    	data := Message{http.StatusOK, time.Now().Format(time.RFC850)}
@@ -54,7 +52,7 @@ func status(w http.ResponseWriter, r *http.Request) {
 // Creates an HTTP server using Mux Router.
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/{[a-z]+}", status).
+	r.HandleFunc("/{[a-z]+}", status).     // Regex fails to capture "/a.../a..." but not "/a..."
 	Schemes("http")
 
 	srv := &http.Server{
