d69406a230ea98a61a22c47256c378981d5600fc
Aman <63730225+asingh2Oswego@users.noreply.github.com>
2020-10-22 17:13:15 -0400

    Added DynamoDB functionality

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module csc482/server
 go 1.12
 
 require (
+	github.com/aws/aws-sdk-go v1.35.13 // indirect
 	github.com/gorilla/mux v1.8.0
 	github.com/jamespearly/loggly v0.0.0-20190206220007-041558ec1c7c // indirect
 )
--- server.go
+++ server.go
@@ -1,24 +1,124 @@
 package main
 
 import (
-    "encoding/json"
-    "net/http"
-    "time"
-    "github.com/gorilla/mux"
-    "strconv"
-    "net"
-    loggly "github.com/jamespearly/loggly"
+    	"encoding/json"
+    	"net/http"
+    	"github.com/gorilla/mux"
+	"strconv"
+	"net"
+	"time"
+	"github.com/aws/aws-sdk-go/aws"
+    	"github.com/aws/aws-sdk-go/aws/session"
+    	"github.com/aws/aws-sdk-go/service/dynamodb"
+    	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+    	loggly "github.com/jamespearly/loggly"
 )
 
+// Status for DynamoDB table.
 type Message struct {
-	Status int
-	Time string
+	Table       string	
+	RecordCount int64	`json:"RecordCount"`
+}
+
+type Information struct {
+	Date string `json:"date"`
+	Base string `json:"base"`
+	
+	Currency struct {
+		USD float64 `json:"USD"`
+		GBP float64 `json:"GBP"`
+		INR float64 `json:"INR"`
+		CAD float64 `json:"CAD"`
+		AUD float64 `json:"AUD"`
+	} `json:"rates"`
 }
 
 // Returns HTTP status codes, pushes notifications to Loggly
 func status(w http.ResponseWriter, r *http.Request) {
     var tag string
     tag = "CSC482GoServer"
+    	
+    	// DynamoDB session.
+	sess := session.Must(session.NewSession(&aws.Config{
+		Region:   aws.String("us-east-1"),
+		Endpoint: aws.String("https://dynamodb.us-east-1.amazonaws.com"),
+	}))
+	
+    	db := dynamodb.New(sess)
+    	
+    	// Loggly Client
+	client := loggly.New(tag)
+	w.Header().Set("Content-Type", "application/json")
+	
+	//Get the Client IP:
+	ip, port, err := net.SplitHostPort(r.RemoteAddr)
+	if err != nil {
+        	client.Send("error", "Could not retrieve client's IP address.")
+	}
+    	
+    	// Using a method other than "Get"
+    if r.Method != http.MethodGet {
+    	client.EchoSend("warn", "Method: " + r.Method + ", Host: " + ip + ":" + port +  ", Requested Path: " + r.URL.Path + ", HTTP Status Code: " 	+ strconv.Itoa(http.StatusMethodNotAllowed) + ".")
+    	
+	w.WriteHeader(http.StatusMethodNotAllowed)
+	data := "405 - Method Not Allowed."
+	json.NewEncoder(w).Encode(data)
+    } else { 
+    	client.EchoSend("info", "Method: " + r.Method + ", Host: " + ip + ":" + port +  ", Requested Path: " + r.URL.Path + ", HTTP Status Code: " + strconv.Itoa(http.StatusOK) + ".")
+    	
+    	// Scan the entire asingh2-rates table.
+    	var tableName string = "asingh2-rates"
+    	input := &dynamodb.ScanInput{
+    		TableName: aws.String(tableName),
+    	}
+    	
+    	result, err := db.Scan(input)
+    	
+    	// Scan request error output.
+    	if err != nil {
+    		client.Send("error", "Could not scan table inside func status " + tableName + " No Echo.")
+    	} else {
+   		// JSON response to client.
+		data := Message{tableName, *result.Count}
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(data)
+    	}
+    }
+}
+
+// Dispays error when an attempt to access non-existent path is made.
+func forbidden(w http.ResponseWriter, r *http.Request) {
+    	var tag string
+    	tag = "CSC482GoServer"
+    
+	client := loggly.New(tag)
+	
+	// Find client's IP address and Port number.
+	ip, port, err := net.SplitHostPort(r.RemoteAddr)
+	if err != nil {
+        	client.Send("error", "Could not retrieve client's IP address.")
+	}
+	
+    	client.EchoSend("info", "Method: " + r.Method + ", Host: " + ip + ":" + port +  ", Requested Path: " + r.URL.Path + ", HTTP Status Code: " + strconv.Itoa(http.StatusNotFound) + ".")
+    	
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusNotFound)
+	data := "404 - Page Not Found."
+	json.NewEncoder(w).Encode(data)
+}
+
+// Return everything from DynamoDB table in a JSON body to client.
+func all(w http.ResponseWriter, r *http.Request) {
+    var tag string
+    tag = "CSC482GoServer"
+    
+    	// DynamoDB session.
+	sess := session.Must(session.NewSession(&aws.Config{
+		Region:   aws.String("us-east-1"),
+		Endpoint: aws.String("https://dynamodb.us-east-1.amazonaws.com"),
+	}))
+	
+    	db := dynamodb.New(sess)
     
 	client := loggly.New(tag)
 	w.Header().Set("Content-Type", "application/json")
@@ -31,33 +131,56 @@ func status(w http.ResponseWriter, r *http.Request) {
     	
     if r.Method != http.MethodGet {
     	client.EchoSend("warn", "Method: " + r.Method + ", Host: " + ip + ":" + port +  ", Requested Path: " + r.URL.Path + ", HTTP Status Code: " 	+ strconv.Itoa(http.StatusMethodNotAllowed) + ".")
+    	
 	w.WriteHeader(http.StatusMethodNotAllowed)
 	data := "405 - Method Not Allowed."
 	json.NewEncoder(w).Encode(data)
     } else {
-    	if r.URL.Path != "/status" {
-    		client.EchoSend("info", "Method: " + r.Method + ", Host: " + ip + ":" + port +  ", Requested Path: " + r.URL.Path + ", HTTP Status Code: " + strconv.Itoa(http.StatusNotFound) + ".")
-		w.WriteHeader(http.StatusNotFound)
-		data := "404 - Page Not Found."
-		json.NewEncoder(w).Encode(data)
-    	} else {
-    	    	client.EchoSend("info", "Method: " + r.Method + ", Host: " + ip + ":" + port +  ", Requested Path: " + r.URL.Path + ", HTTP Status Code: " + strconv.Itoa(http.StatusOK) + ".")
-	    	data := Message{http.StatusOK, time.Now().Format(time.RFC850)}
-	    	w.WriteHeader(http.StatusOK)
-		json.NewEncoder(w).Encode(data)
+    	client.EchoSend("info", "Method: " + r.Method + ", Host: " + ip + ":" + port +  ", Requested Path: " + r.URL.Path + ", HTTP Status Code: " + strconv.Itoa(http.StatusOK) + ".")
+    	
+    	// Instance of information struct for unmarshaling JSON.
+    	var info []Information
+    	
+    	// Scan the entire asingh2-rates table.
+    	var tableName string = "asingh2-rates"
+    	input := &dynamodb.ScanInput{
+    		TableName: aws.String(tableName),
+    	}
+    	
+    	result, err := db.Scan(input)
+    	
+    	if err != nil {
+   		client.Send("error", "Could not perform scan on table " + tableName + " in func all. No Echo.")
+   	} else {
+   		// Unmarshal all resulting scan data into Information struct.
+   		err := dynamodbattribute.UnmarshalListOfMaps(result.Items, &info)
+   		if err != nil {
+   			client.EchoSend("error", "Couldn't unmarshal scan data in func all. No echo.")
+   		}
+   		// Encode data as JSON and return with query.
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(info)
 	}
     }
-}
+} 
 
 // Creates an HTTP server using Mux Router.
 func main() {
+	
+	// Handle all possible routes and invalid ones.
 	r := mux.NewRouter()
-	r.HandleFunc("/{[a-z]+}", status).     // Regex fails to capture "/a.../a..." but not "/a..."
+	r.HandleFunc("/asingh2/status", status)
+	r.HandleFunc("/asingh2/status/", status)
+	r.HandleFunc("/asingh2/all", all)
+	r.HandleFunc("/asingh2/all/", all)
+	r.HandleFunc("/{[a-z]+}", forbidden)
+	r.HandleFunc("/{[a-z]+}/", forbidden)
+	r.HandleFunc("/{[a-z]+}/{[a-z]+}", forbidden).
 	Schemes("http")
 
 	srv := &http.Server{
 		Handler: r,
-	    	Addr:	":8000",
+	    	Addr:	":8080",
 	    	
 	    	WriteTimeout: 15 * time.Second,
 	    	ReadTimeout: 15 * time.Second,
